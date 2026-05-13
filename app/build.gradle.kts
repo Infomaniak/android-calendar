ca2e54e8a07eee416178a97fa01c68c45a7e1ff4
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-05-13 10:39:42 +0200

    chore: Fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.infomaniak.calendar"
     compileSdk {
-        version = release(37)
+        version = release(36)
     }
 
     defaultConfig {
@@ -29,9 +29,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
-    }
-    kotlinOptions {
-        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
