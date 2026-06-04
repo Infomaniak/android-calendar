fb2f88bc492588590fcde3b23622bea22e85502f
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-06-04 10:35:08 +0200

    feat: Show Calendar events
    # Conflicts:
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/CalendarTestViewModel.kt
    #	multiplatform-calendar
    
    # Conflicts:
    #	multiplatform-calendar
    
    # Conflicts:
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/CalendarTestUiState.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/CalendarTestViewModel.kt
    
    # Conflicts:
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/CalendarTestScreenContent.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/composable/Loaded.kt
    
    # Conflicts:
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/CalendarTestViewModel.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/composable/Detail.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/composable/EventCard.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/composable/Loaded.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/model/CalendarUi.kt
    #	app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/previewParameter/CalendarTestUiStatePreviewProvider.kt
    #	multiplatform-calendar

---
+++ app/src/main/java/com/infomaniak/calendar/ui/screen/calendarTest/model/EventExt.kt
@@ -0,0 +1,54 @@
+/*
+ * Infomaniak Calendar - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.calendar.ui.screen.calendarTest.model
+
+import com.infomaniak.multiplatform_calendar.core.domain.model.event.Event
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.format
+import kotlinx.datetime.format.char
+import kotlin.time.ExperimentalTime
+
+
+private val dateTimeFormatter = LocalDateTime.Format {
+    date(LocalDate.Formats.ISO)
+    char(' ')
+    hour(); char(':'); minute()
+}
+
+@OptIn(ExperimentalTime::class)
+internal fun Event.toUi(): EventUi = EventUi(
+    id = id.url,
+    title = title.ifBlank { "(no title)" },
+    timeRange = timeRange(isAllDay, start, end),
+    location = location?.takeIf { it.isNotBlank() },
+    status = status?.takeIf { it.isNotBlank() },
+    categories = categories?.takeIf { it.isNotBlank() },
+    description = description?.takeIf { it.isNotBlank() },
+    lastModified = lastModified?.format(dateTimeFormatter),
+)
+
+private fun timeRange(
+    isAllDay: Boolean,
+    start: LocalDateTime?,
+    end: LocalDateTime?,
+): String? = when {
+    isAllDay -> "All day"
+    start != null && end != null -> "${start.format(dateTimeFormatter)} → ${end.format(dateTimeFormatter)}"
+    else -> null
+}
