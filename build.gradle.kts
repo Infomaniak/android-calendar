29fccbba406c8302a4586dc3e611f4a7d4883fc0
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-06-11 15:23:25 +0200

    feat: Define app versions inside rootProject.extra like other projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("appCompileSdk", 36)
+        set("appMinSdk", 27)
+        set("javaVersion", JavaVersion.VERSION_21)
+    }
+}
+
 plugins {
     alias(core.plugins.android.application) apply false
     alias(core.plugins.compose.compiler) apply false
