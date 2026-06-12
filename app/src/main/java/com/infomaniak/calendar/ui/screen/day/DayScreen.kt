39d7939a1e7119da06cff83e6ed99fbafd4d7ec8
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2026-06-12 11:23:54 +0200

    feat: Add custom scaffold with menu icon

--- app/src/main/java/com/infomaniak/calendar/ui/screen/day/DayScreen.kt
+++ app/src/main/java/com/infomaniak/calendar/ui/screen/day/DayScreen.kt
@@ -20,9 +20,7 @@ package com.infomaniak.calendar.ui.screen.day
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
