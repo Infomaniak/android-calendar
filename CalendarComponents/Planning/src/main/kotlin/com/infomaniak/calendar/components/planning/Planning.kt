c6f774d6bcea1f674ce1c9f0c40d156e79099850
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-06-19 08:51:27 +0200

    chore: Remove remembered today instance to be sure it gets updated sooner

--- CalendarComponents/Planning/src/main/kotlin/com/infomaniak/calendar/components/planning/Planning.kt
+++ CalendarComponents/Planning/src/main/kotlin/com/infomaniak/calendar/components/planning/Planning.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,7 +53,7 @@ fun Planning(
     modifier: Modifier = Modifier,
     lazyListState: LazyListState = rememberLazyListState(),
 ) {
-    val today = remember { Clock.System.todayIn(TimeZone.currentSystemDefault()) }
+    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
 
     LazyColumn(
         state = lazyListState,
