51c84768357b110ad1028c5a52989cf282b2483a
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-06-24 13:01:07 +0200

    feat: Rollback to sync ids

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         versionName = "1.0"
 
         // TODO[login]: Replace this placeholder with the real Calendar OAuth client id once available.
-        buildConfigField("String", "CLIENT_ID", "\"E90BC22D-67A8-452C-BE93-28DA33588CA4\"")
+        buildConfigField("String", "CLIENT_ID", "\"CE011334-F75A-4263-9F9F-45FC5A142F59\"")
 
         androidResources {
             localeFilters += listOf("en", "de", "es", "fr", "it", "da", "el", "fi", "nb", "nl", "pl", "pt", "sv")
