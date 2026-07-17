4451cb00a653088b6990f18befb41aeff063bee5
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-07-17 09:51:46 +0200

    feat: Add missing space between day sections

--- CalendarComponents/Planning/src/main/kotlin/com/infomaniak/calendar/components/planning/Planning.kt
+++ CalendarComponents/Planning/src/main/kotlin/com/infomaniak/calendar/components/planning/Planning.kt
@@ -20,7 +20,9 @@ package com.infomaniak.calendar.components.planning
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -95,6 +97,10 @@ fun Planning(
                             is EventUi.TodayEmptyState -> TodayEmptyState(onClick = goToEventCreation, Modifier.fillMaxWidth())
                         }
                     }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.height(Margin.Medium))
                 }
             }
         }
