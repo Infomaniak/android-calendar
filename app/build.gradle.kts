d50704609c9e339b285cd0810c8978993b075219
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-07-21 10:23:19 +0200

    chore: Remove useless dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -185,8 +185,6 @@ dependencies {
     implementation(kmpCalendar.kotlinx.datetime)
 
     implementation(libs.haze)
-    implementation(libs.haze.blur)
-    implementation(libs.haze.materials)
 
     // Navigation 3
     implementation(core.androidx.lifecycle.viewmodel.navigation3)
