ecdd3b12e250a8a287f0a49f59b31a4ca0cbe718
Sol Rubado <sol.rubado@infomaniak.com>
2026-07-31 16:58:09 +0200

    feat: Add menu list and menu items composables

---
+++ app/src/main/java/com/infomaniak/calendar/ui/component/drawer/MenuItem.kt
@@ -0,0 +1,89 @@
+/*
+ * Infomaniak Calendar - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.calendar.ui.component.drawer
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.infomaniak.calendar.R
+import com.infomaniak.calendar.ui.theme.CalendarThemeForPreview
+import com.infomaniak.core.ui.compose.margin.Margin
+
+@Composable
+fun MenuItem(menuOption: MenuOption, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = menuOption.itemAction,
+                role = Role.Button
+            )
+            .padding(all = Margin.Small),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Margin.Medium),
+    ) {
+        Icon(
+            painter = painterResource(menuOption.itemIcon),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+        Column(
+            modifier = Modifier.weight(1f),
+        ) {
+            Text(
+                text = stringResource(menuOption.itemNameRes),
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+        Icon(
+            painter = painterResource(R.drawable.ic_chevron_right),
+            contentDescription = null,
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MenuItemPreview() {
+    CalendarThemeForPreview {
+        MenuItem(
+            menuOption = MenuOption(
+                itemNameRes = R.string.accountManagement,
+                itemIcon = R.drawable.ic_circle_user,
+                itemAction = {},
+            ),
+        )
+    }
+}
