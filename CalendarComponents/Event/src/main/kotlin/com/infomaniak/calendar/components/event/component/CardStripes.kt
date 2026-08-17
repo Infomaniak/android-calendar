ab415a89f1e2733dbf2549be2d4bd8e462967a97
Sol Rubado <sol.rubado@infomaniak.com>
2026-08-17 17:49:33 +0200

    fix: Add more spaces between lines in maybe event design

--- CalendarComponents/Event/src/main/kotlin/com/infomaniak/calendar/components/event/component/CardStripes.kt
+++ CalendarComponents/Event/src/main/kotlin/com/infomaniak/calendar/components/event/component/CardStripes.kt
@@ -33,7 +33,7 @@ import kotlin.math.hypot
 import kotlin.math.sin
 
 private val STRIPE_WIDTH = 8.dp
-private val STRIPE_SPACING = 2.dp
+private val STRIPE_SPACING = 4.dp
 
 @Composable
 internal fun Modifier.cardStripes(status: EventItemStatus): Modifier {
