f115cbbcb296641c23e95b2006af08d40eeb2026
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-09-14 11:41:10 +0200

    perf: Optimize animated chevron rotation using graphicsLayer

--- CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/component/DescriptionCollapsableButton.kt
+++ CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/component/DescriptionCollapsableButton.kt
@@ -22,6 +22,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.plus
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -34,7 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextLayoutResult
@@ -99,7 +100,7 @@ private fun AnimatedChevron(isCollapsed: () -> Boolean) {
     Icon(
         painterResource(R.drawable.ic_chevron_down),
         contentDescription = null,
-        modifier = Modifier.rotate(animatedChevronRotation.value),
+        modifier = Modifier.graphicsLayer { rotationZ = animatedChevronRotation.value },
     )
 }
 
