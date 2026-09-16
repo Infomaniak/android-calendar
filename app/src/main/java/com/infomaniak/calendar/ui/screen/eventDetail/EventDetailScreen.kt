2764ae43dbca3f1d8eaccd1506d207f729d4c19e
Sol Rubado <sol.rubado@infomaniak.com>
2026-09-16 15:40:46 +0200

    fix: Remove unnecessary imports

--- app/src/main/java/com/infomaniak/calendar/ui/screen/eventDetail/EventDetailScreen.kt
+++ app/src/main/java/com/infomaniak/calendar/ui/screen/eventDetail/EventDetailScreen.kt
@@ -20,7 +20,6 @@ package com.infomaniak.calendar.ui.screen.eventDetail
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.plus
@@ -32,7 +31,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
