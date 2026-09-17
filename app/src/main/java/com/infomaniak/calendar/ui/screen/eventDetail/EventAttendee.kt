23a5303b2be4d3faf9f4af15b620f968da220b93
Sol Rubado <sol.rubado@infomaniak.com>
2026-09-17 13:27:36 +0200

    fix: Remove unnecessary imports

--- app/src/main/java/com/infomaniak/calendar/ui/screen/eventDetail/EventAttendee.kt
+++ app/src/main/java/com/infomaniak/calendar/ui/screen/eventDetail/EventAttendee.kt
@@ -17,8 +17,6 @@
  */
 package com.infomaniak.calendar.ui.screen.eventDetail
 
-import android.R.attr.fontWeight
-import android.R.attr.maxLines
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
