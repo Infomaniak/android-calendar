cd97de9cd0f05d06b7f5b4a7f6489e0f084e8ff6
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2026-09-22 14:02:52 +0200

    chore: Bump material3 version

--- CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/detail/component/KMeetButton.kt
+++ CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/detail/component/KMeetButton.kt
@@ -20,6 +20,7 @@ package com.infomaniak.calendar.components.eventdetail.detail.component
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -73,6 +74,7 @@ internal fun KMeetButton(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun CopyButton(onClick: () -> Unit) {
     val contentDescription = stringResource(RCommon.string.buttonCopy)
