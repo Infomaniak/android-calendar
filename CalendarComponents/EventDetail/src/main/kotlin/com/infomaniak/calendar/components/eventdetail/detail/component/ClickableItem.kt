89def49578580a0ea032daa99137d6da4f7a5ae0
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2026-09-22 14:31:36 +0200

    fix: Use correct vertical padding in 'ClickableItem'

--- CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/detail/component/ClickableItem.kt
+++ CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/detail/component/ClickableItem.kt
@@ -31,10 +31,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.infomaniak.calendar.components.resources.R
 import com.infomaniak.core.ui.compose.margin.Margin
 
 internal val LIST_ITEM_HORIZONTAL_PADDING = Margin.Medium
+internal val LIST_ITEM_VERTICAL_PADDING = 10.dp
 
 @Composable
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -76,8 +78,10 @@ internal fun ClickableItem(
         leadingContent = leadingContent,
         trailingContent = trailingContent,
         onClick = onClick,
-        contentPadding = contentPadding + PaddingValues(horizontal = LIST_ITEM_HORIZONTAL_PADDING),
         shapes = ListItemDefaults.RectangleShapes,
+        contentPadding = contentPadding + PaddingValues(
+            horizontal = LIST_ITEM_HORIZONTAL_PADDING, vertical = LIST_ITEM_VERTICAL_PADDING,
+        ),
     )
 }
 
