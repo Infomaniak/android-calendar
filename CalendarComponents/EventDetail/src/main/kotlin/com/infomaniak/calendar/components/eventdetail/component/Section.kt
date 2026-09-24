5e27e4210a22b11508681ed01c51bd7fb11664bc
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-09-24 14:17:36 +0200

    refactor: Extract section component to its own file and update divider padding with new specs

---
+++ CalendarComponents/EventDetail/src/main/kotlin/com/infomaniak/calendar/components/eventdetail/component/Section.kt
@@ -0,0 +1,59 @@
+/*
+ * Infomaniak Calendar - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.calendar.components.eventdetail.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+
+/**
+ * Automatically shows or hides divider based on if any content is composed or not. This layout acts like a column.
+ */
+@Composable
+internal fun Section(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(),
+    content: @Composable () -> Unit,
+) {
+    Layout(
+        contents = listOf(
+            { HorizontalDivider(modifier = Modifier.padding(contentPadding)) },
+            content,
+        ),
+        modifier = modifier,
+    ) { (dividerMeasurables, contentMeasurables), constraints ->
+        if (contentMeasurables.isEmpty()) return@Layout layout(0, 0) {}
+
+        val childConstraints = constraints.copy(minHeight = 0)
+        val placeables = (dividerMeasurables + contentMeasurables).map { it.measure(childConstraints) }
+
+        val width = placeables.maxOf { it.width }.coerceIn(constraints.minWidth, constraints.maxWidth)
+        val height = placeables.sumOf { it.height }.coerceIn(constraints.minHeight, constraints.maxHeight)
+
+        layout(width, height) {
+            var y = 0
+            placeables.forEach { placeable ->
+                placeable.place(0, y)
+                y += placeable.height
+            }
+        }
+    }
+}
